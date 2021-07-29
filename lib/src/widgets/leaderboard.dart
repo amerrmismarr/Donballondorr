90a053f40e4072101b5ceeb283d3c87114f7c740
Amer-Mesmar <amerrmismarr@gmail.com>
2021-07-29 21:45:14 +0200

    Progresss commit

--- lib/src/widgets/leaderboard.dart
+++ lib/src/widgets/leaderboard.dart
@@ -100,12 +100,10 @@ class _LeaderBoardState extends State<LeaderBoard> {
                    subtitle:Text(usersProfilesList[index]['email'], style: TextStyle(
                      fontSize: 10,
                      color: Colors.grey[300]),) ,
-                   trailing: Row(
-                     children: [
-                       //Icon(Icons.),
+                   trailing:
                        Text(usersProfilesList[index]['score'].toString() 
-                       ),
-                     ],
+                       
+                     ,
                    ),
                  ),
                );
